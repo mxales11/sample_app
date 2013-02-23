1ddb154bf7ba9aaa5fb52041ffd8ba15a8b72354
mxales11 <mxales11@smumn.edu>
2013-02-23 13:59:23 -0600

    making all tests passinf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user, only: [:edit, :update]
+
+
+
+  def index
+    @users = User.all
+  end
+
 
   def new
   	@user = User.new
@@ -43,8 +50,12 @@ class UsersController < ApplicationController
   private
 
   def signed_in_user
-    redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
   end
+ 
 
 
   def correct_user
