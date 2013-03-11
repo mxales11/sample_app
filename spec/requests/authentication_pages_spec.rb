5136614ba37357344600c0d39cd3ba272e2593cd
mxales11 <mxales11@smumn.edu>
2013-03-11 01:43:13 -0500

    final following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -46,6 +46,24 @@ describe "Authentication" do
 			let(:user) { FactoryGirl.create(:user) }
 
 
+
+			describe "in the Relationships controller" do
+				describe "submitting to the create action" do
+					before { post relationships_path }
+					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete relationships_path(1) }
+					specify { response.should redirect_to(signin_path) }
+				end
+			end
+
+
+
+
+
+
 			describe "in the Microposts controller" do
 
 				describe "submitting to the create action" do
@@ -99,6 +117,18 @@ describe "Authentication" do
 					before { visit users_path }
 					it { should have_selector('title', text: 'Sign in')}
 				end
+
+
+				describe "visiting the following page" do
+					before { visit following_user_path(user) }
+					it { should have_selector('title', text: 'Sign in') }
+				end
+
+				describe "visiting the followers page" do
+					before { visit followers_user_path(user) }
+					it { should have_selector('title', text: 'Sign_in') }
+				end
+
 			end
 		end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,8 +17,8 @@ subject {page}
 		describe "for signed-in users" do
 			let(:user) { FactoryGirl.create(:user) }
 			before do
-				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+				FactoryGirl.create(:micropost, user: user, content: "Lorem")
+				FactoryGirl.create(:micropost, user: user, content: "Ipsum")
 				signed_in user
 				visit root_path
 			end
@@ -28,6 +28,19 @@ subject {page}
 					page.should have_selector("li##{item.id}", text: item.content)
 				end
 			end
+
+
+			describe "follower/following counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 follower", href: followers_user_path(user)) }
+			end 
+
 		end
 
 	end
